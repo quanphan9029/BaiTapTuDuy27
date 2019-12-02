a8d03cc78b44bc44df2a3c393f8aea32d9717725
quanphan9029 <55967910+quanphan9029@users.noreply.github.com>
2019-12-02 18:36:00 +0700

    Add files via upload

--- Buoi4/src/com/company/Bai6.java
+++ Buoi4/src/com/company/Bai6.java
@@ -1,4 +1,39 @@
 package com.company;
 
+<<<<<<< HEAD
+import java.util.Scanner;
+
 public class Bai6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        final int randomNum = (int) (Math.random() * 999 + 1);
+        int inputNum, guess_turn = 0;
+        do {
+            System.out.println("Mời bạn đoán và nhập vào: ");
+            inputNum = Integer.parseInt(sc.nextLine());
+
+            while (inputNum <= 0 || inputNum > 1000) {
+                System.out.println("Số nguyên nhập vào phải từ 1 - 1000.");
+                System.out.println("Mời nhập lại:");
+                inputNum = Integer.parseInt(sc.nextLine());
+            }
+
+            if (inputNum < randomNum) {
+                System.out.println("Nhập số lớn hơn.");
+                guess_turn++;
+            }
+            else if (inputNum > randomNum) {
+                System.out.println("Nhập số nhỏ hơn.");
+                guess_turn++;
+            }
+        } while (inputNum != randomNum);
+        System.out.println("Bạn đã đoán đúng.");
+        System.out.println("Sau " + guess_turn + " lượt.");
+
+
+    }
+=======
+public class Bai6 {
+>>>>>>> 54ae30fa0248bf3e2277a5e95947dad1f3b40c14
 }
--- Buoi4/src/com/company/Bai7.java
+++ Buoi4/src/com/company/Bai7.java
@@ -1,4 +1,80 @@
 package com.company;
 
+<<<<<<< HEAD
+import java.net.SecureCacheResponse;
+import java.util.Scanner;
+
 public class Bai7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int player_option, count_player_win = 0, count_com_win = 0;
+
+        // Menu
+        do {
+            int com_option = (int) (Math.random() * 3 + 1);
+            menu();
+            System.out.print("Mời bạn nhập lựa chọn: ");
+            player_option = Integer.parseInt(sc.nextLine());
+            while (player_option > 3 || player_option < 0) {
+                System.out.println("Lựa chọn bạn nhập không phù hợp.");
+                menu();
+                System.out.print("Mời nhập lại: ");
+                player_option = Integer.parseInt(sc.nextLine());
+            }
+
+            if (player_option == com_option) {
+                System.out.println("Hòa.");
+            }
+            else if (player_option == 1 && com_option == 2) {
+                System.out.println("Bạn thắng.");
+                count_player_win++;
+                count_com_win++;
+            }
+            else if (player_option == 1 && com_option == 3) {
+                System.out.println("Bạn thua.");
+                count_com_win++;
+            }
+            else if (player_option == 2 && com_option == 1) {
+                System.out.println("Bạn thua.");
+                count_com_win++;
+            }
+            else if (player_option == 2 && com_option == 3) {
+                System.out.println("Bạn thắng.");
+                count_player_win++;
+            }
+            else if (player_option == 3 && com_option == 1) {
+                System.out.println("Bạn thắng.");
+                count_player_win++;
+            }
+            else {
+                System.out.println("Bạn thua.");
+                count_com_win++;
+            }
+        } while (player_option != 0);
+
+        tySo(count_player_win, count_com_win);
+    }
+    public static void menu() {
+        System.out.println("Menu lựa chọn:");
+        System.out.println("0. Thoát.");
+        System.out.println("1. Bao.");
+        System.out.println("2. Búa.");
+        System.out.println("3. Kéo.");
+    }
+
+    public static void tySo(int count_player_win, int count_com_win) {
+        if (count_com_win < count_player_win) {
+            System.out.println("Bạn thắng chung cuộc với tỷ số " + count_player_win + " - " + count_com_win);
+        }
+        else if (count_com_win > count_player_win) {
+            System.out.println("Máy thắng chung cuộc với tỷ số " + count_com_win + " - " + count_player_win);
+        }
+        else {
+            System.out.println("Bạn và Máy hòa nhau với tỷ số " + count_player_win + " - " + count_com_win);
+        }
+    }
+=======
+public class Bai7 {
+>>>>>>> 54ae30fa0248bf3e2277a5e95947dad1f3b40c14
 }
